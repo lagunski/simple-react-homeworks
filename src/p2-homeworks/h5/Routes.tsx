f68a8a6f49c3d750a91fe3934afcae42c71cbb48
lagunski <lagunski@gmail.com>
2020-11-14 17:16:20 +0100

    HW5(1)

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,27 +1,40 @@
 import React from "react";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+import JuniorPlus from "./pages/JuniorPlus";
+import Junior from "./pages/Junior";
+import { Switch, Redirect, Route } from "react-router-dom";
 
 export const PATH = {
     PRE_JUNIOR: "/pre-junior",
+    JUNIOR: "/junior",
+    JUNIOR_PLUS: "/junior-plus",
     // add paths
 }
 
 function Routes() {
     return (
         <div>
+
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            <Switch>
 
-            {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
-            {/*<Route path={"/"} exact render={() => <Redirect to={PRE_JUNIOR}/>}/>*/}
+                {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
+                {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
+                <Route path={"/"} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
 
-            {/*<Route path={PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+                <Route path={"/"} exact render={() => <Redirect to={PATH.JUNIOR}/>}/>
+                <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
+
+                <Route path={"/"} exact render={() => <Redirect to={PATH.JUNIOR_PLUS}/>}/>
+                <Route path={PATH.JUNIOR_PLUS} render={() => <JuniorPlus/>}/>
                 // add routes
 
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+                {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route render={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     );
 }
